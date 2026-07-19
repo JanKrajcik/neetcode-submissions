ba2dd7dc739041d75ff4877af1d5df8dfc9a7f59
Ján Krajčík <128795284+JanKrajcik@users.noreply.github.com>
2026-07-19 22:54:16 +0200

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;  // <value, index>
+        for (int i = 0; i < nums.size(); i++) {
+            // Diff between target and i-th number.
+            int need = target - nums[i];
+            // Try to find the number equal to the iDiff, so it does not have to be fetched twice.
+            if (seen.find(need) != seen.end()) {
+                return {seen[need], i};
+            } else {
+                seen[nums[i]] = i;
+            }
+        }
+    }
+};
